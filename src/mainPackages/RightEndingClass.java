905bf126397a06a86153186f5f17a49e38301c5c
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-06-18 19:16:01 +0900

    오른쪽 엔딩 완성

--- src/mainPackages/RightEndingClass.java
+++ src/mainPackages/RightEndingClass.java
@@ -1,4 +1,8 @@
 package mainPackages;
+
+import basePackages.BaseFunc;
+import basePackages.ImgClass;
+
 //오른쪽 엔딩 관련 클래스를 모아 두는 곳 입니다.
 //들어 가야 할것들
 //- 선택하는 메소드(이 클래스의 중심인 클래스)-담당 : ?? (메소드 이름 : rightMethod)
@@ -7,11 +11,112 @@ package mainPackages;
 //- 아파트 친구(연우)집(11)(진엔딩)-담당 : 박선주 (메소드 이름 : floorFriendMethod)
 //- 아파트 옥상-담당 : 권하은 (메소드 이름 : rooftopMethod)
 public class RightEndingClass {
-	public void houseMethod() {
+	public static void houseMethod() {
 		
 	}
-	public void floorFriendMethod() {
+	public static void floorFriendMethod() {
+		BaseFunc.margin20();
+		BaseFunc.countStr("(친구집 앞에 도착했다.)");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countChNoLn("(띵-");
+		BaseFunc.countCh1("똥)");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countStr("(벨을 누르고 친구를 불렀다.)");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countCh2("▷ 연우야...");
+		BaseFunc.countStrNoLn("연우: ");
+		BaseFunc.countCh2("\"누구세요??\"");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countStr("(집안에서 인기척이 들리고 친구의 대답소리가 들려온다.)");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countCh2("▷ 다행이다...");
+		////////////////////////////////딜레이
+		BaseFunc.margin20();
+		BaseFunc.count();
+		BaseFunc.count();
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		ImgClass.RadMaskImg3();
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.countStr("나 예뻐...? 나 예뻐...? 나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?나 예뻐...? 나 예뻐...?");
+		BaseFunc.margin20();
 		
+		//////////////////////////////엔딩
+		BaseFunc.countCh2("     실종 청소년 정보         ");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		//###주인공 이름
+		BaseFunc.countStr("       (주인공 이름)       ");
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		//###주인공 나이
+		BaseFunc.countStr("       나이 : 18       ");
+		BaseFunc.countStr("       성별 :        ");
+		////////////////////////////////
+		////////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.margin20();
+		BaseFunc.countStrNoLn("유진 : ");
+		BaseFunc.countCh2("혹시 (주인공이름) 이야기 들었어?");
+		BaseFunc.countCh2("그저께 갑자기 실종됐는데, 그날 야자 끝나고 아무도 못봤다는데...?");
+		///////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countStrNoLn("\n연우 : ");
+		BaseFunc.countCh2("아..근데 내가 그날 밖에서 (주인공이름) 목소리가 들려서 나가봤었거든..?");
+		BaseFunc.countCh2("근데 아무도 없었어...");
+		BaseFunc.countCh2("무슨일이 있엇던걸까...?");
+		///////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.countCh2("잘못들은 걸까...?");
+		///////////////////////////////딜레이
+		BaseFunc.count();
+		BaseFunc.count();
+		BaseFunc.count();
+		BaseFunc.count();
+		///////////////////////////////딜레이
+		BaseFunc.margin20();
+		BaseFunc.countCh1("▷ (흑..흑..)"); //주인공
+		BaseFunc.margin20();
+		BaseFunc.countCh2("The End");
 	}
 	
 }
