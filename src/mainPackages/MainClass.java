cfb54db228473dbcd5fd0bc33445eb00a371f34b
yscho010612@gmail.com <yscho010612@gmail.com>
2022-06-19 22:56:17 +0900

    프로토 타입 와성 \^@^/

--- src/mainPackages/MainClass.java
+++ src/mainPackages/MainClass.java
@@ -1,17 +1,14 @@
 package mainPackages;
-//mainClass  
 import basePackages.*;
 
 public class MainClass {
 
 	public static void main(String[] args){
-<<<<<<< HEAD
+
 	//	PrologueClass.prologueCho3Method();
 	//	RightEndingClass.rightMethod();
-		LeftEndingClass.policeMethod();
-=======
-		LeftEndingClass.churchMethod();
->>>>>>> branch 'kwonha' of https://github.com/yes25cho/RadMask.git
+//		LeftEndingClass.policeMethod();
+//		LeftEndingClass.churchMethod();
 		HomeClass.homeIn();
 		
 	}
